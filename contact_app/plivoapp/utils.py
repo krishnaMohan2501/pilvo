56a76d734c1093b1f2ef40a6c818f6c92f219e76
pycoder123 <kykrishna24@gmail.com>
2019-02-08 14:47:03 +0530

    adding a decorator for authentication

--- contact_app/plivoapp/utils.py
+++ contact_app/plivoapp/utils.py
@@ -15,3 +15,20 @@ class JSONResponse(HttpResponse):
             status=status,
             content_type=content_type,
         )
+        
+def authenticate_request(*params):
+    def decorater_maker(fn):
+        @functools.wraps(fn)
+        def wrapper(request, *args, **kwargs):
+            if isinstance(request, HttpRequest):
+                request_dict = request.__getattribute__(request.method)
+                for param in params:
+                    if not request_dict.get(param, None):
+                        return render_error_response(
+                            400,
+                            "Missing field '%s' in request" % param)
+            return fn(request, *args, **kwargs)
+
+        return wrapper
+
+    return decorater_maker 
