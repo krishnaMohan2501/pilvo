6a26f48dd410fd4604d43fc8659d71245c6be209
pycoder123 <kykrishna24@gmail.com>
2019-02-08 14:37:28 +0530

    url fixes

--- contact_app/contact_app/urls.py
+++ contact_app/contact_app/urls.py
@@ -13,8 +13,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^contact_app/$', 'plivoapp.views.new_contact', name='new_contact'),
-    url(r'^edit/(?P<contact_id>\d+)/$', 'plivoapp.views.edit_contact', name='edit_contact'),
-    url(r'^delete/(?P<contact_id>\d+)/$', 'plivoapp.views.delete_contact', name='delete_contact'),
+    url(r'^edit/$', 'plivoapp.views.edit_contact', name='edit_contact'),
+    url(r'^delete/$', 'plivoapp.views.delete_contact', name='delete_contact'),
     url(r'^search/$', 'plivoapp.views.search_term')
     
 )
