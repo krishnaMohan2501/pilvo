1e51d0131116c731139bcf9ff57e88a136f96efe
pycoder123 <kykrishna24@gmail.com>
2019-02-08 14:10:11 +0530

    Update urls.py

--- contact_app/contact_app/urls.py
+++ contact_app/contact_app/urls.py
@@ -1,6 +1,6 @@
 
-#from django.conf.urls import patterns, include, url
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
+#from django.conf.urls.defaults import *
 
 
 from django.contrib import admin
