2030247df536edfae72509553185512c4c645c9f
pycoder123 <kykrishna24@gmail.com>
2019-02-08 14:38:25 +0530

    Update urls.py

--- contact_app/contact_app/urls.py
+++ contact_app/contact_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^contact_app/$', 'plivoapp.views.new_contact', name='new_contact'),
+    url(r'^add/$', 'plivoapp.views.new_contact', name='new_contact'),
     url(r'^edit/$', 'plivoapp.views.edit_contact', name='edit_contact'),
     url(r'^delete/$', 'plivoapp.views.delete_contact', name='delete_contact'),
     url(r'^search/$', 'plivoapp.views.search_term')
